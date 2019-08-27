a5c789621ba2f0a1404bbdad4f4f26155851fe88
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-08-27 15:22:39 -0600

    add [act]: function to generate successive blocks

--- writers_block/helpers.py
+++ writers_block/helpers.py
@@ -11,3 +11,15 @@ def make_genesis_block():
         data='initial commit',
         prev_hash='0'
     )
+
+
+def make_next_block(prev_block, data=None):
+
+    index     = prev_block.index + 1
+    timestamp = datetime.datetime.now()
+    next_hash = prev_block.hash
+
+    if not data:
+        data = f'This is block #{index}'
+
+    return Block(index, timestamp, data, next_hash)
