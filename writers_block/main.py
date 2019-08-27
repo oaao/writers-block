c1410725a2880ada35cf1c375c39ddd1ee701300
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-08-27 15:38:30 -0600

    add: HTTP server

--- writers_block/main.py
+++ writers_block/main.py
@@ -1,3 +1,29 @@
+from flask import Flask, request
+
 from helpers import make_genesis_block, make_next_block, initialize_chain
 
-new_blockchain = initialize_chain(25)
+
+node = Flask(__name__)
+
+node_transactions = []
+
+
+@node.route('/transaction', methods=['POST'])
+def transaction():
+
+    new_transaction = request.get_json()
+
+    node_transactions.append(new_transaction)
+
+    print(
+        f'----- NEW TRANSACTION RECEIVED ----\n'
+        f'FROM:   {new_transaction["from"]}\n'
+        f'TO:     {new_transaction["to"]}\n'
+        f'AMOUNT: {new_transaction["amount"]}\n'
+    )
+
+    return "transaction successfully submitted"
+
+node.run()
+
+# new_blockchain = initialize_chain(25)
